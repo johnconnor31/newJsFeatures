b77e0dc9b68e20404757b701c1ce275101462d0c
sairam s <sairamsingireesu@gmail.com>
2019-12-30 20:07:34 +0530

    Updates

---
+++ precache-manifest.0736a2a96fbb8f5df83bc242383716d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb5bbff507c4eadb6bd46c0b26fbf86f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "626a69c340ffca5f8b71",
+    "url": "/static/css/main.7621a935.chunk.css"
+  },
+  {
+    "revision": "66122e6545a91f656364",
+    "url": "/static/js/2.0f771e00.chunk.js"
+  },
+  {
+    "revision": "626a69c340ffca5f8b71",
+    "url": "/static/js/main.4e59f49c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
