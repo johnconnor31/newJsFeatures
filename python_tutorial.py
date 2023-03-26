2b709b9750d7b9f17bbe0edd381e0de327d2615d
johnconnor31 <sairamsingireesu@gmail.com>
2023-03-26 15:57:08 +0530

    python tutorial

---
+++ python_tutorial.py
@@ -0,0 +1,39 @@
+number = 10
+print('input is', number);
+if(number > 0):
+	print('Saanukoola sankhya')
+elif(number == 0):
+	print('Sunna')
+else:
+	print('Pratikoola sankhya')
+
+for i in range(5):
+	print(i)
+
+i = 0	
+while i > -5 :
+	print(i)
+	i-=1
+
+if(i < 0):
+	1
+
+def add_no(n1, n2):
+	return n1
+
+print(add_no(n2=1, n1=2))
+print(pow(2, 3))
+
+constGreet = lambda: print('Hemlo world')
+constGreet()
+
+greet = lambda where, name: print('Hey {}, {}'.format(where,name))
+greet('there', "Delilah!")
+
+print(complex(3))
+
+# array slicing
+arr = [1, 2, 3]
+print(arr[1:2])
+print('ac' is 'ac')
+print(f'abc is {arr}')
